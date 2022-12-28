ebce8016dd6b85562b2ff2744e3021fe877eb618
Tom Bourjala <tom.bourjala@gmail.com>
2022-12-28 23:33:31 +0100

    feat: working json data fetch

--- build.gradle.kts
+++ build.gradle.kts
@@ -35,6 +35,8 @@ dependencies {
     implementation("com.google.code.gson:gson:2.9.1")
 
     implementation("org.jetbrains.kotlin:kotlin-reflect:1.7.20")
+
+    implementation("org.json:json:20210307")
 }
 
 testing {
--- src/main/kotlin/org/isen/gasfinder/App.kt
+++ src/main/kotlin/org/isen/gasfinder/App.kt
@@ -1,5 +1,8 @@
 package org.isen.gasfinder
 
+import org.json.JSONArray
+import java.net.URL
+
 class App {
     val greeting: String
         get() {
@@ -9,4 +12,11 @@ class App {
 
 fun main() {
     println(App().greeting)
+
+    val url = "https://data.economie.gouv.fr/explore/dataset/prix-carburants-fichier-instantane-test-ods-copie/download/?format=json&timezone=Europe/Berlin&lang=fr"
+    val jsonData = URL(url).readText()
+    val records = JSONArray(jsonData)
+    val firstRecord = records.getJSONObject(0)
+    val fields = firstRecord.getJSONObject("fields")
+    println(fields)
 }
