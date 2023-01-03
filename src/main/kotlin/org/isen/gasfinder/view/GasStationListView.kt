3255feec9f0dcca76339a637fd8cad247a68a65a
Tom Bourjala <tom.bourjala@gmail.com>
2023-01-03 12:56:20 +0100

    feat: updated gas station list view

--- src/main/kotlin/org/isen/gasfinder/view/GasStationListView.kt
+++ src/main/kotlin/org/isen/gasfinder/view/GasStationListView.kt
@@ -1,52 +1,27 @@
+
 package org.isen.gasfinder.view
 
 
 
 import org.apache.logging.log4j.kotlin.Logging
 import org.isen.gasfinder.controller.GasStationController
-import org.isen.gasfinder.model.Fields
 import org.isen.gasfinder.model.GasStation
-import org.isen.gasfinder.model.Record
 import java.awt.BorderLayout
 import java.awt.Dimension
-import java.awt.GridLayout
-import java.awt.Image
 import java.awt.event.ActionEvent
 import java.awt.event.ActionListener
 import java.beans.PropertyChangeEvent
-import java.lang.IllegalStateException
-import javax.swing.Action
-import javax.swing.DefaultComboBoxModel
-import javax.swing.ImageIcon
-import javax.swing.JComboBox
-import javax.swing.JFrame
-import javax.swing.JLabel
-import javax.swing.JPanel
-import javax.swing.JScrollPane
-import javax.swing.WindowConstants
-import javax.swing.border.EmptyBorder
+import javax.swing.*
 
 
 class GasStationListView (val controller: GasStationController):IGasStationView, ActionListener {
 
     companion object : Logging
 
-    private var gasStationList: JComboBox<Record> = JComboBox<Record>().apply {
-        this.addActionListener(this@GasStationListView)
-        this.renderer = StationInfoCellRender()
-    }
-
-
+    private var gasStationList: JList<GasStation> = JList()
     private val frame: JFrame
-    private val labelStationSelected_CP = JLabel()
-    private val labelStationSelected_Pop = JLabel()
-    private val labelStationSelected_GasName = JLabel()
-    private val labelStationSelected_GasPrice = JLabel()
-    private val labelStationSelected_Services = JLabel()
-    private val labelStationSelected_City = JLabel()
 
     init {
-
         frame = JFrame().apply {
             isVisible = false
             contentPane = makeGUI()
@@ -63,46 +38,17 @@ class GasStationListView (val controller: GasStationController):IGasStationView,
         val contentPane = JPanel()
 
         contentPane.layout = BorderLayout()
-        contentPane.add(createVelibComboBox(), BorderLayout.NORTH)
-        contentPane.add(createStationInformationPanel(), BorderLayout.WEST)
+        contentPane.add(makeNorthArea(), BorderLayout.NORTH)
 
         return contentPane
     }
 
-
-    private fun createVelibComboBox(): JPanel {
-        val contentPane = JPanel()
-        contentPane.layout = BorderLayout()
-        gasStationList.border = EmptyBorder(5, 5, 5, 5)
-        contentPane.add(gasStationList, BorderLayout.CENTER)
-
-        return contentPane
-    }
-
-    private fun createStationInformationPanel(): JPanel {
-        val contentPane = JPanel()
-        contentPane.layout = GridLayout(6, 2)
-        contentPane.preferredSize = Dimension(250, 100)
-
-        contentPane.add(JLabel("ville: "))
-        contentPane.add(labelStationSelected_City)
-
-        contentPane.add(JLabel("code postal: "))
-        contentPane.add(labelStationSelected_CP)
-
-        contentPane.add(JLabel("Route ou Autoroute : "))
-        contentPane.add(labelStationSelected_Pop)
-
-        contentPane.add(JLabel("Nom du carburant : "))
-        contentPane.add(labelStationSelected_GasName)
-
-        contentPane.add(JLabel("Prix du carburant: "))
-        contentPane.add(labelStationSelected_GasPrice)
-
-        contentPane.add(JLabel("Service de la Station : "))
-        contentPane.add(labelStationSelected_Services)
-
-        return contentPane
+    private fun makeNorthArea(): JPanel {
+        val result = JPanel()
+        result.layout = BorderLayout()
+        result.add(JLabel("Station :"), BorderLayout.WEST)
+        result.add(gasStationList, BorderLayout.CENTER)
+        return result
     }
 
     override fun display() {
@@ -114,36 +60,22 @@ class GasStationListView (val controller: GasStationController):IGasStationView,
     }
 
     override fun propertyChange(evt: PropertyChangeEvent) {
-        if (evt.newValue is GasStation) {
+        if (evt.newValue is List<*>){
             logger.info("receive GasStationInformation data")
-            gasStationList.model = DefaultComboBoxModel<Record>((evt.newValue as GasStation).records.toTypedArray())
-
-                    labelStationSelected_City.text = (gasStationList.selectedItem as Record).fields.ville
-                    labelStationSelected_CP.text = (gasStationList.selectedItem as Record).fields.cp
-                    labelStationSelected_Pop.text =
-                        if ((gasStationList.selectedItem as Record).fields.pop == "R") "Route" else "Autoroute"
-                    labelStationSelected_GasName.text = (gasStationList.selectedItem as Record).fields.prix_nom
-                    labelStationSelected_GasPrice.text =
-                        (gasStationList.selectedItem as Record).fields.prix_valeur.toString()
-                    labelStationSelected_Services.text = (gasStationList.selectedItem as Record).fields.services_service
-                }
-            else {
-                logger.info("unknown data")
-            }
+            val list = evt.newValue as List<GasStation>
+            val model = DefaultListModel<GasStation>()
+            list.forEach { model.addElement(it) }
+            gasStationList.model = model
+            gasStationList.cellRenderer = StationInfoCellRender()
         }
 
 
     override fun actionPerformed(e: ActionEvent) {
-        if (e.source is JComboBox<*>) {
-            logger.info(
-                "click on combo with index: ${gasStationList.selectedIndex} and "
-                        + "value ${gasStationList.selectedItem}"
-            )
-            controller.selectedStation(gasStationList.model.getElementAt(gasStationList.selectedIndex).fields.id)
-        }
+
     }
 
 }
+
 
 
 
--- src/main/kotlin/org/isen/gasfinder/view/StationInfoCellRender.kt
+++ src/main/kotlin/org/isen/gasfinder/view/StationInfoCellRender.kt
@@ -1,7 +1,6 @@
 package org.isen.gasfinder.view
 
-import org.isen.gasfinder.model.Record
-import java.awt.Color
+import org.isen.gasfinder.model.GasStation
 import java.awt.Component
 import java.awt.Image
 import javax.swing.ImageIcon
@@ -9,34 +8,25 @@ import javax.swing.JLabel
 import javax.swing.JList
 import javax.swing.ListCellRenderer
 
-class StationInfoCellRender:JLabel(), ListCellRenderer<Record> {
+class StationInfoCellRender:JLabel(), ListCellRenderer<GasStation> {
 
     init {
         super.setOpaque(true)
     }
 
     override fun getListCellRendererComponent(
-        list: JList<out Record> ?,
-        value: Record?,
-        index:Int,
+        list: JList<out GasStation>?,
+        value: GasStation?,
+        index: Int,
         isSelected: Boolean,
         cellHasFocus: Boolean
-    ):Component {
-    if(isSelected) {
-        background = Color.ORANGE
-        foreground = list?.foreground
-    } else {
-        background = list?.background
-        foreground = list?.foreground
-    }
-
-       /* val img = ImageIcon(this::class.java.getResource(("/tag.png"))).let {
-            ImageIcon(it.image.getScaledInstance(5,5, Image.SCALE_DEFAULT))
-        }*/
-
-
-        if (value != null) text = "${value.fields.id} - ${value.fields.adresse} "
-        return this
+    ): Component {
+        val component = this
+        if (value is GasStation) {
+            text = "${value.geoPoint.address}\n${value.geoPoint.city}"
+            icon = img
+        }
+        return component
     }
 
 }
