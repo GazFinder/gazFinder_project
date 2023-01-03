68d3d7d6b5a2982529d14d615b48dfdad3658e5c
Tom Bourjala <tom.bourjala@gmail.com>
2023-01-03 13:03:13 +0100

    feat: geopoint implementation

--- src/main/kotlin/org/isen/gasfinder/model/GeoPoint.kt
+++ src/main/kotlin/org/isen/gasfinder/model/GeoPoint.kt
@@ -1,4 +1,14 @@
 package org.isen.gasfinder.model
 
-class GeoPoint {
+class GeoPoint(
+    val latitude: Double,
+    val longitude: Double,
+    val address: String? = null,
+    val city: String? = null,
+    val postalCode: String? = null
+) {
+
+    override fun toString(): String {
+        return "GeoPoint(latitude=$latitude, longitude=$longitude, address=$address, city=$city, postalCode=$postalCode)"
+    }
 }
