b695c02d2aa75d9a44784938eaf46e6e887da140
Tom Bourjala <tom.bourjala@gmail.com>
2023-01-03 13:03:50 +0100

    feat: updated gasStation class

--- src/main/kotlin/org/isen/gasfinder/model/GasStation.kt
+++ src/main/kotlin/org/isen/gasfinder/model/GasStation.kt
@@ -1,30 +1,60 @@
 package org.isen.gasfinder.model
 
-import com.github.kittinunf.fuel.core.ResponseDeserializable
-import com.google.gson.Gson
+class GasStation(
+    val geoPoint: GeoPoint,
+    val name: String? = null,
+    val gas: List<Gas>,
+    val services: List<Service>,
+    val isOnHighway: Boolean = false,
+    val id: String
+) {
+    var distance: Double? = null
 
-data class GasStation( val records:List<Record>) {
+    override fun toString(): String {
+        return "GasStation(geoPoint=$geoPoint, name=$name, gas=$gas, services=$services, isOnHighway=$isOnHighway, id=$id, distance=$distance)"
+    }
 
-    class Deserializer: ResponseDeserializable<GasStation> {
-        override fun deserialize(content: String): GasStation {
-            return Gson().fromJson(content, GasStation::class.java)
+    class Gas(val id: String, val name: String, val price: Double) {
+        override fun toString(): String {
+            return "$name, $price"
+        }
+    }
 
+
+    enum class Service(val value: String) {
+        SELL_GAS( "Vente de gaz domestique (Butane, Propane)"),
+        AIR_PUMP( "Station de gonflage"),
+        ATM("DAB (Distributeur automatique de billets)"),
+        FOOD_STORE("Boutique alimentaire"),
+        TRUCK_PARKING( "Piste poids lourds"),
+        ATM24("Automate CB 24/24"),
+        NON_FOOD_STORE("Boutique non alimentaire"),
+        AUTOMATIC_WASH("Lavage automatique"),
+        CASH_MACHINE("Carburant additivé"),
+        PUBLIC_TOILETS("Toilettes publiques"),
+        MANUAL_WASH("Lavage manuel"),
+        TAKE_AWAY( "Restauration à emporter"),
+        CAR_RENTAL("Location de véhicule"),
+        PARCEL_DELIVERY( "Relais colis"),
+        LAUNDRY( "Laverie"),
+        ADDITIVES( "Vente d'additifs carburants"),
+        RESTAURANT( "Restauration sur place"),
+        REPAIR( "Services réparation / entretien"),
+        WIFI("Wifi"),
+        HEATING_OIL( "Vente de fioul domestique"),
+        LAMP_OIL ("Vente de pétrole lampant"),
+        BABY_AREA( "Espace bébé"),
+        BAR("Bar"),
+        ELECTRICITY( "Bornes électriques"),
+        SHOWER("Douches"),
+        CAMPER_PARKING("Aire de camping-cars"),
+        GNV("GNV"),
+        UNKNOWN("Unknown");
+    }
+
+    companion object {
+        fun getServiceFromString(service: String): Service {
+            return Service.values().find { it.value == service } ?: Service.UNKNOWN
         }
     }
 }
-
-data class Record(
-    val fields:Fields
-)
-data class Fields(
-    val adresse: String,
-    val cp: String,
-    val geom: List<Double>,
-    val id: String,
-    val pop: String,
-    val prix_id: String,
-    val prix_nom: String,
-    val prix_valeur: Double,
-    val services_service: String,
-    val ville: String
-)
