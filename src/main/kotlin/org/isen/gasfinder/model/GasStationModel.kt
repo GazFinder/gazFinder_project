0dade1c6fa4ec16d4455db44ace25e7664e671cb
Tom Bourjala <tom.bourjala@gmail.com>
2023-01-04 13:08:10 +0100

    feat: updated datatypes

--- src/main/kotlin/org/isen/gasfinder/model/GasStationModel.kt
+++ src/main/kotlin/org/isen/gasfinder/model/GasStationModel.kt
@@ -13,19 +13,20 @@ class GasStationModel: IGasStationModel {
 
     private var searchResultGasStations: List<GasStation> by Delegates.observable(emptyList()) { _, _, _ ->
         logger.info("update search result gas stations")
-        pcs.firePropertyChange(IGasStationModel.DATATYPE_STATION, "searchResultGasStations", searchResultGasStations)
-    }
-
-    private var search: StationSearch? by Delegates.observable(null) { _, _, _ ->
-        logger.info("update search")
-        pcs.firePropertyChange(IGasStationModel.DATATYPE_STATION, "search", search)
+        pcs.firePropertyChange(IGasStationModel.DATATYPE_STATIONS, null, searchResultGasStations)
     }
 
     private var selectedStation:GasStation? by Delegates.observable(null) {
             _, oldValue, newValue ->
         logger.info("update selectedStation")
-        pcs.firePropertyChange(IGasStationModel.DATATYPE_STATION, oldValue, newValue)
+        pcs.firePropertyChange(IGasStationModel.DATATYPE_STATION_SELECTED, oldValue, newValue)
     }
+
+    private var search: StationSearch? by Delegates.observable(null) { _, _, _ ->
+        logger.info("update search")
+        pcs.firePropertyChange(IGasStationModel.DATATYPE_SEARCH, null, search)
+    }
+
 
 
 
@@ -36,7 +37,9 @@ class GasStationModel: IGasStationModel {
 
     override fun register(datatype: String?, listener: PropertyChangeListener) {
         if(datatype == null) {
-            pcs.addPropertyChangeListener(IGasStationModel.DATATYPE_STATION, listener)
+            pcs.addPropertyChangeListener(IGasStationModel.DATATYPE_STATIONS, listener)
+            pcs.addPropertyChangeListener(IGasStationModel.DATATYPE_STATION_SELECTED, listener)
+            pcs.addPropertyChangeListener(IGasStationModel.DATATYPE_SEARCH, listener)
             //pcs.addPropertyChangeListener(IGasStationModel.DATATYPE_CARTO, listener)
         } else {
             pcs.addPropertyChangeListener(datatype, listener)
--- src/main/kotlin/org/isen/gasfinder/model/IGasStationModel.kt
+++ src/main/kotlin/org/isen/gasfinder/model/IGasStationModel.kt
@@ -4,8 +4,9 @@ import java.beans.PropertyChangeListener
 
 interface IGasStationModel {
     companion object{
-        const val DATATYPE_STATION = "station"
-        const val DATATYPE_CARTO = "carto"
+        const val DATATYPE_STATIONS = "stations"
+        const val DATATYPE_STATION_SELECTED = "stationSelected"
+        const val DATATYPE_SEARCH = "search"
     }
     enum class DataSources (val urlStart: String, val urlEnd: String, val dataType: String) {
         DATAECO("https://data.economie.gouv.fr/api/records/1.0/search/?dataset=prix-carburants-fichier-instantane-test-ods-copie&q=", "&rows=-1&facet=id&facet=cp&facet=pop&facet=adresse&facet=ville&facet=geom&facet=prix_id&facet=prix_valeur&facet=prix_nom&facet=services_service", "JSON"),
