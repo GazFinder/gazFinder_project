a2190883ded08fe932bd8d9a7460c9073980fc14
Milena Rastad <milena.rastad@gmail.com>
2023-01-04 17:07:48 +0100

    StationInoCellRenderer

--- src/main/kotlin/org/isen/gasfinder/view/StationInfoCellRender.kt
+++ src/main/kotlin/org/isen/gasfinder/view/StationInfoCellRender.kt
@@ -3,21 +3,27 @@ package org.isen.gasfinder.view
 import org.isen.gasfinder.model.GasStation
 import java.awt.Color
 import java.awt.Component
+import java.awt.GridLayout
 import javax.swing.JLabel
 import javax.swing.JList
 import javax.swing.ListCellRenderer
 import java.awt.Image
 import javax.swing.ImageIcon
+import javax.swing.SwingConstants
 
 class StationInfoCellRender:JLabel(), ListCellRenderer<GasStation> {
 
     init {
         super.setOpaque(true)
-    }
 
+    }
     val img = ImageIcon(this::class.java.getResource(("/station_icon.png"))).let {
         ImageIcon(it.image.getScaledInstance(50, 50, Image.SCALE_DEFAULT))
+
     }
+
+
+
     override fun getListCellRendererComponent(
         list: JList<out GasStation>?,
         value: GasStation?,
@@ -34,7 +40,7 @@ class StationInfoCellRender:JLabel(), ListCellRenderer<GasStation> {
         }
         val component = this
         if (value is GasStation) {
-            text = value.getSearchableString()
+            text = "<HTML><h3>${value.geoPoint.address?.lowercase()} ${value.geoPoint.city?.uppercase()} ${value.geoPoint.postalCode}</h3></HTML>"
             icon = img
         }
         return component
